c398b3394b02d012f5feda02408827026461a284
Patrick Evers <patrickevers2@web.de>
2019-10-23 10:13:55 +0200

    Die URLs für die Bilder werden erst geladen bevor die Bilder angezeigt werden.

--- home.js
+++ home.js
@@ -3,9 +3,10 @@ var baseURL = 'https://jsonplaceholder.typicode.com';
 var photos = 'photos/'
 
 //Wie viele Bilder sollen geladen werden?
-var imgNumber=20;
+var imgNumber=25;
 
 //Lade Bilder
+let urls = [];
 for(var i = 1; i<=imgNumber; i++){
     //Hole URL für aktuelles Bild
     var anhang=photos+i;
@@ -15,13 +16,17 @@ for(var i = 1; i<=imgNumber; i++){
     let url = fetch(test)
     .then(response => response.json())
     .then(json => json.url)
-    .then( (out) => {
+    urls[i-1] = url;
+}
+
+//Zeige Bilder an
+for(var i = 0; i < imgNumber; i++){
+    urls[i].then(function(result){
         var img = document.createElement("img");
-        img.src = out;
+        img.src = result;
         img.id = "image";
         img.height="100"
         img.alt = "Image";
         document.getElementById('output').appendChild(img);
     })
 }
-
