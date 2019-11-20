9797653d787f361aff5298fdf87323f3f5625ac2
Patrick Evers <patrickevers2@web.de>
2019-11-20 13:59:57 +0100

    Erstellung einer TExtdatei mit den Namen der Bilder

--- home.js
+++ home.js
@@ -1,11 +1,16 @@
+const fs = require('fs').promises;
+
 //Wie viele Bilder sollen geladen werden?
 var imgNumber=25;
+
 
 //Lade Bilder
 async function getPhotos(){
     let response = await fetch('https://jsonplaceholder.typicode.com/photos');
     let data = await response.json();
-
+    var photoName;
+    var content = "";   
+    
     for(var i = 0; i < imgNumber; i++)
     {
         var img = document.createElement("img");
@@ -14,6 +19,12 @@ async function getPhotos(){
         img.height="100"
         img.alt = "Image";
         document.getElementById('output').appendChild(img);
+        photoName = data[i].url.substring(data[i].url.lastIndexOf('/')+1);
+        content += photoName + "\n";
     }
+  
+    await fs.writeFile('photoNames.txt', content, 'utf8');    
 }
 getPhotos();
+
+
